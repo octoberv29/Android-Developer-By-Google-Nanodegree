34bd59265ded1dcbf8f1e32eb3ee4fcc58426600
Samat <samat.alpyssov@gmail.com>
2020-05-20 14:19:42 +0600

    Finished Places

---
+++ advanced-android-app-development/Lesson05-Shush-Me/AdvancedAndroid_Shushme-T0X.04-Exercise-Geofencing/app/src/main/java/com/example/android/shushme/Geofencing.java
@@ -0,0 +1,141 @@
+package com.example.android.shushme;
+
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Result;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceBuffer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Geofencing implements ResultCallback {
+    // COMPLETED (1) Create a Geofencing class with a Context and GoogleApiClient constructor that
+    // initializes a private member ArrayList of Geofences called mGeofenceList
+
+    public static final String TAG = Geofencing.class.getSimpleName();
+    private static final float GEOFENCE_RADIUS = 50; // 50 meters
+    private static final long GEOFENCE_TIMEOUT = 24 * 60 * 60 * 1000; // 24 hours
+
+    private GoogleApiClient mGoogleApiClient;
+    private Context mContext;
+
+    private PendingIntent mGeofencePendingIntent;
+    private List<Geofence> mGeofenceList;
+
+
+    public Geofencing(GoogleApiClient googleApiClient, Context context) {
+        this.mGoogleApiClient = googleApiClient;
+        this.mContext = context;
+        mGeofencePendingIntent = null;
+        mGeofenceList = new ArrayList<>();
+    }
+
+    // COMPLETED (2) Inside Geofencing, implement a public method called updateGeofencesList that
+    // given a PlaceBuffer will create a Geofence object for each Place using Geofence.Builder
+    // and add that Geofence to mGeofenceList
+
+    public void updateGeofencesList(PlaceBuffer places) {
+        mGeofenceList = new ArrayList<>();
+        if (places == null || places.getCount() == 0) return;
+        for (Place place : places) {
+            // Read the place information from the DB cursor
+            String placeUID = place.getId();
+            double placeLat = place.getLatLng().latitude;
+            double placeLng = place.getLatLng().longitude;
+            // Build a Geofence object
+            Geofence geofence = new Geofence.Builder()
+                    .setRequestId(placeUID)
+                    .setExpirationDuration(GEOFENCE_TIMEOUT)
+                    .setCircularRegion(placeLat, placeLng, GEOFENCE_RADIUS)
+                    .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER | Geofence.GEOFENCE_TRANSITION_EXIT)
+                    .build();
+            // Add it to the list
+            mGeofenceList.add(geofence);
+        }
+    }
+
+    // COMPLETED (3) Inside Geofencing, implement a private helper method called getGeofencingRequest that
+    // uses GeofencingRequest.Builder to return a GeofencingRequest object from the Geofence list
+
+    private GeofencingRequest getGeofencingRequest() {
+        GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
+        builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER);
+        builder.addGeofences(mGeofenceList);
+        return builder.build();
+    }
+
+    // COMPLETED (4) Create a GeofenceBroadcastReceiver class that extends BroadcastReceiver and override
+    // onReceive() to simply log a message when called. Don't forget to add a receiver tag in the Manifest
+
+    // COMPLETED (5) Inside Geofencing, implement a private helper method called getGeofencePendingIntent that
+    // returns a PendingIntent for the GeofenceBroadcastReceiver class
+
+    private PendingIntent getGeofencePendingIntent() {
+        // Reuse the PendingIntent if we already have it.
+        if (mGeofencePendingIntent != null) {
+            return mGeofencePendingIntent;
+        }
+        Intent intent = new Intent(mContext, GeofenceBroadcastReceiver.class);
+        mGeofencePendingIntent = PendingIntent.getBroadcast(mContext, 0, intent, PendingIntent.
+                FLAG_UPDATE_CURRENT);
+        return mGeofencePendingIntent;
+    }
+
+    // COMPLETED (6) Inside Geofencing, implement a public method called registerAllGeofences that
+    // registers the GeofencingRequest by calling LocationServices.GeofencingApi.addGeofences
+    // using the helper functions getGeofencingRequest() and getGeofencePendingIntent()
+
+    public void registerAllGeofences() {
+        // Check that the API client is connected and that the list has Geofences in it
+        if (mGoogleApiClient == null || !mGoogleApiClient.isConnected() ||
+                mGeofenceList == null || mGeofenceList.size() == 0) {
+            return;
+        }
+        try {
+            LocationServices.GeofencingApi.addGeofences(
+                    mGoogleApiClient,
+                    getGeofencingRequest(),
+                    getGeofencePendingIntent()
+            ).setResultCallback(this);
+        } catch (SecurityException securityException) {
+            // Catch exception generated if the app does not use ACCESS_FINE_LOCATION permission.
+            Log.e(TAG, securityException.getMessage());
+        }
+    }
+
+    // COMPLETED (7) Inside Geofencing, implement a public method called unRegisterAllGeofences that
+    // unregisters all geofences by calling LocationServices.GeofencingApi.removeGeofences
+    // using the helper function getGeofencePendingIntent()
+
+    public void unRegisterAllGeofences() {
+        if (mGoogleApiClient == null || !mGoogleApiClient.isConnected()) {
+            return;
+        }
+        try {
+            LocationServices.GeofencingApi.removeGeofences(
+                    mGoogleApiClient,
+                    // This is the same pending intent that was used in registerGeofences
+                    getGeofencePendingIntent()
+            ).setResultCallback(this);
+        } catch (SecurityException securityException) {
+            // Catch exception generated if the app does not use ACCESS_FINE_LOCATION permission.
+            Log.e(TAG, securityException.getMessage());
+        }
+    }
+
+    @Override
+    public void onResult(@NonNull Result result) {
+        Log.e(TAG, String.format("Error adding/removing geofence : %s",
+                result.getStatus().toString()));
+    }
+}
